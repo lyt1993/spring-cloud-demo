59cb53596389f5b618c2165b3384caa8701d2d7e
lyt1993 <554199576@qq.com>
2018-06-05 23:11:28 +0800

    Initial commit

---
+++ spring-cloud-ribbon-server/src/main/java/com/yaotai/demo/RibbonServerApplication.java
@@ -0,0 +1,24 @@
+package com.yaotai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonServerApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
